dcf355f2d9fc73ea298a5ed1262f9aecd37a93bf
torvic <130111@unsaac.edu.pe>
2021-11-22 11:08:42 -0500

    Feat: Add word counter feature

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			counter: 0
+		}
+	}
+	handleChange = (e) => {
+		const arr = e.target.value.split('')
+		this.setState({
+			counter: arr.length
+		})
+	}
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.handleChange}></textarea>
+        <div className="counter">{this.state.counter}</div>
       </div>
     );
   }
